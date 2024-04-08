314587d552a8aedbdcf74ed2838f06877a838bd3
luchosangui <89468428+luchosangui@users.noreply.github.com>
2024-04-08 16:34:12 -0300

    domain package agregado (vacio)

--- BuildingManager/Domain/Apartment.cs
+++ BuildingManager/Domain/Apartment.cs
@@ -12,12 +12,12 @@ namespace Domain
         public Guid Id { get; private set; }
         public int Floor { get; set; }
         public int Number { get; set; }
-        public Owner Owner { get; set; }
+        public User Owner { get; set; }
         public int NumberOfBedrooms { get; set; }
         public int NumberOfBathrooms { get; set; }
         public bool HasTerrace { get; set; }
 
-        public Apartment(int floor, int number, Owner owner, int numberOfBedrooms, int numberOfBathrooms, bool hasTerrace)
+        public Apartment(int floor, int number, User owner, int numberOfBedrooms, int numberOfBathrooms, bool hasTerrace)
         {
             Id = Guid.NewGuid();
             Floor = floor;
